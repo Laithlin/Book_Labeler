b6327bb96665137e1d64389878a69f8d81e386f1
Laithlin <justynasikorska15@gmail.com>
2025-03-08 10:31:05 +0100

    check urls, check if appropriate urls were found

--- labeler.py
+++ labeler.py
@@ -39,10 +39,24 @@ def get_books_names():
 def search_in_google(books):
     urls = []
     for book in books:
+        start_url_len = len(urls)
+
         for j in search(book, tld="co.in", num=2, stop=2, pause=5):
-            # print(j)
-            urls.append(j)
+            if(check_url(j)):
+                urls.append(j)
+            continue
+
+        end_url_len = len(urls)
+        if(start_url_len == end_url_len):
+            print("didn't find good url")
     return urls
+
+def check_url(url):
+    print(url)
+    contain_string = "https://lubimyczytac.pl/ksiazka/"
+    if contain_string in url:
+        return True
+    return False
 
 def get_book_category(response):
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -51,35 +65,14 @@ def get_book_category(response):
     category = category_href.get_text()
     print(category)
 
-# to search
-# query = "Geeksforgeeks"
-
-# for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-#     print(j)
-
-
 library = get_books_names()
 # print(library[0])
-# url = search_in_google([library[0]])
-url = search_in_google([library[0]])
-res = requests.get(url[0])
-# print(response.text)
-# print(url)
-# print(directory_list)
-# print("Files:")
-# print(library)
+url = search_in_google(library)
+print("linki:\n")
+print(url)
+# res = requests.get(url[0])
 
-get_book_category(res)
-# soup = BeautifulSoup(response.text, 'html.parser')
-# category_href = soup.find("a", "book__category d-sm-block d-none")
-# # print(category_href)
-# category = category_href.get_text()
-# print(category)
-# try:
-#     with open("./file.txt", "x") as f:
-#         f.write(response.text)
-# except FileExistsError:
-#     print("Already exists.")
+# get_book_category(res)
 
 
 
