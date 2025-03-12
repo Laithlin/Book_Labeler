df827f0d1fe385bcdb43adc62165913bfa48856e
Laithlin <justynasikorska15@gmail.com>
2025-03-12 19:59:50 +0100

    search in lubimyczytac

--- labeler.py
+++ labeler.py
@@ -37,7 +37,8 @@ def get_books_names():
     for _, _, files in os.walk(path_to_books, topdown=False):
         for file in files:
             # print(file)
-            books.append(cut_extension(file) + " lubimyczytac")
+            # books.append(cut_extension(file) + " lubimyczytac")
+            books.append(cut_extension(file))
         bar.next()
 
     bar.finish()
@@ -73,7 +74,7 @@ def search_in_bing(books):
         url = urllib.parse.urlunparse(('https', 'bing.com', '/search', '', urllib.parse.urlencode({"q": book}), ""))
         # print(query)
         print(url)
-        custom_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0"
+        custom_user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
         res = requests.get(url, headers={"User-Agent": custom_user_agent})
 
         # print(res.status_code)
@@ -81,14 +82,42 @@ def search_in_bing(books):
         # # print(res.text)
         soup = BeautifulSoup(res.text, 'html.parser')
         urls = soup.find_all("a")
-        print(urls)
-        # for url in urls:
-        #     print(url["href"])
+        # print(urls)
+        for link in urls:
+            # print(link.get('href'))
+            if(check_url(link.get('href'))):
+                print(link.get('href'))
+
+def search_lubimyczytac(books): 
+    for book in books:
+        url = urllib.parse.urlunparse(('https', 'lubimyczytac.pl', '/szukaj/ksiazki', '', urllib.parse.urlencode({"phrase": book}), ""))
+        print(url)
+        res = requests.get(url)
+        # print(res.text)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        urls = soup.find_all("a")
+        # print(urls)
+        for link in urls:
+            link_href = link.get('href')
+            if(link_href == None):
+                print(link_href)
+            else:
+                # print(link_href + " check")
+                check_url(link_href)
+            # if(check_url(link.get('href'))):
+            #     print(link.get('href'))
+
+# TODO stworzyc oddzielna funkcje do wyciagania tytulu ksiazki
+# jak bede miala tytul to latwo wyszukac odpowiedni url
+
+# TODO zrobic porzadek w kodzie bo juz sie gubie co jest od czego
 
 def check_url(url):
     # print(url)
-    contain_string = "https://lubimyczytac.pl/ksiazka/"
+    # contain_string = "https://lubimyczytac.pl/ksiazka/"
+    contain_string = "/ksiazka/"
     if contain_string in url:
+        print(url)
         return True
     return False
 
@@ -104,8 +133,9 @@ def create_file_for_links(data):
         json.dump(data, outfile, ensure_ascii=False)
 
 library = get_books_names()
+search_lubimyczytac(library)
 # # print(library[0])
-search_in_bing(library)
+# search_in_bing(library)
 # url, lib = search_in_google(library)
 # print(lib)
 # create_file_for_links(lib)
